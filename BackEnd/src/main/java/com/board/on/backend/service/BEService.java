bd087cba34ffce352f6a99497118d3e4a0775802
RudyLuvIt <dukejang01@gmail.com>
2024-07-08 21:50:32 +0900

    user-process

--- BackEnd/src/main/java/com/board/on/backend/service/BEService.java
+++ BackEnd/src/main/java/com/board/on/backend/service/BEService.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 public class BEService{
     @Autowired
     private BERepository beRepository;  //BE레포지토리를 jpa레포지토리로부터 받아왔기에 그 안의 기능 사용 가능
-    public void write(Gifticon be){ //write라는 메소드를 BE레포지토리안의 save라는 기능으로 지정 그 안에 넣는 것이 엔티티로 받아 온 값
-        beRepository.save(be);
+    public void write(Gifticon gifticon){ //write라는 메소드를 BE레포지토리안의 save라는 기능으로 지정 그 안에 넣는 것이 엔티티로 받아 온 값
+        beRepository.save(gifticon);
     }
     public List<Gifticon> boardList(){
         return beRepository.findAll();
