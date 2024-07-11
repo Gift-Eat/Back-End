babc4144720dbdfc0a1126b6b627e0fa19b6a384
RudyLuvIt <dukejang01@gmail.com>
2024-07-11 19:54:21 +0900

    front-back

---
+++ BackEnd/src/test/java/com/board/on/backend/controller/JsonControllerTest.java
@@ -0,0 +1,28 @@
+package com.board.on.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonControllerTest {
+
+    @Test
+    void createPro() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void updateForm() {
+    }
+}
